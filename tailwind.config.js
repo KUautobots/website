ca8abdee05ba1a2765018f774dfc80823a76d201
Prathamesh Punde(Local) <prathameshpunde02@gmail.com>
2025-01-09 20:22:02 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html','./js/script.js'],
+  theme: {
+    extend: {},
+    colors: {
+      'kwhite': '#F6F6F6',
+      'kblack': '#171717',
+      'kaccent' : '#08AB9F',
+    },
+  },
+  plugins: [],
+}
+
